cbd89776c01d6fc32d9cbff18e3639d62bea5e9c
kenzot11 <r0628842@student.thomasmore.be>
2017-04-07 23:18:14 +0200

    logout word geregistreerd in databank

--- Site/logout.php
+++ Site/logout.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
+include("functions.php");
 
 if(isset($_SESSION['logged_in']))
 {
 	$naam = $_SESSION['name'];
+	user_offline();
 	session_unset();
 	session_destroy();
 	header( "Location: index.php?logout=true&naam=" .$naam );
