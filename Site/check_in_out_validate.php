0608120938a767a72687c8bd9f29409c7c709c6b
kenzot11 <r0628842@student.thomasmore.be>
2017-04-07 23:19:42 +0200

    kleine bug bij het nakijken van in building

--- Site/check_in_out_validate.php
+++ Site/check_in_out_validate.php
@@ -6,7 +6,7 @@ if (isset($_SESSION["logged_in"]))
 {
 	fill_session($_SESSION["user_id"]);
 	$link = connecteren();
-	$query = "SELECT in_building_now FROM in_building WHERE user_id = '" .$_SESSION["user_id"]. "'";
+	$query = "SELECT in_building_now FROM in_building WHERE user_id = '" .$_SESSION["user_id"]. "' ORDER BY time_check DESC";
   	$result= mysqli_query($link, $query) or die("FOUT: er is een fout opgetreden bij het uitvoeren van de query \"$query\"");
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
