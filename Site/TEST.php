651b6010e6de4e31d22f46758e4bbe48999b2d38
kenzot11 <r0628842@student.thomasmore.be>
2017-04-07 23:20:23 +0200

    test page

--- Site/TEST.php
+++ Site/TEST.php
@@ -4,36 +4,41 @@ session_start();
 
 include("functions.php");
 
+user_login();
+
 fill_session($_SESSION["user_id"]);
 
 echo $_SESSION["logged_in"];
+
+/*
+echo $_SESSION["colleague"]["logged_in"];
 echo "<br>";
-echo $_SESSION["name"];
+echo $_SESSION["colleague"]["name"];
 echo "<br>";
-echo $_SESSION["nin"];
+echo $_SESSION["colleague"]["nin"];
 echo "<br>";
-echo $_SESSION["address"];
+echo $_SESSION["colleague"]["address"];
 echo "<br>";
-echo $_SESSION["gender"];
+echo $_SESSION["colleague"]["gender"];
 echo "<br>";
-echo $_SESSION["email"];
+echo $_SESSION["colleague"]["email"];
 echo "<br>";
-echo $_SESSION["profile_picture"];
+echo $_SESSION["colleague"]["profile_picture"];
 echo "<br>";
-echo $_SESSION["in_building"];
+echo $_SESSION["colleague"]["in_building"];
 echo "<br>";
-echo $_SESSION["date_of_birth"];
+echo $_SESSION["colleague"]["date_of_birth"];
 echo "<br>";
-echo $_SESSION["martial_status"];
+echo $_SESSION["colleague"]["martial_status"];
 echo "<br>";
-echo $_SESSION["phone_number"];
+echo $_SESSION["colleague"]["phone_number"];
 echo "<br>";
-echo $_SESSION["function"];
+echo $_SESSION["colleague"]["function"];
 echo "<br>";
-echo $_SESSION["rights"]["check_in_out"];
+echo $_SESSION["colleague"]["rights"]["check_in_out"];
 echo "<br>";
-echo $_SESSION["rights"]["info"];
+echo $_SESSION["colleague"]["rights"]["info"];
 echo "<br>";
-echo $_SESSION["rights"]["messages"];
-echo "<br>";
+echo $_SESSION["colleague"]["rights"]["messages"];
+echo "<br>";*/
 ?>
