9a8dc3524c457077cd6139a95a746e185a73e9c5
kenzot11 <r0628842@student.thomasmore.be>
2017-04-23 19:09:24 +0200

    heb er voor gezorgd dat men geen berichten kan zien die men niet mag zien

--- Site/actual_message.php
+++ Site/actual_message.php
@@ -3,95 +3,116 @@ session_start();
 
 include("functions.php");
 
-if (isset($_SESSION["logged_in"]) && isset($_POST["id"]))
+if (isset($_SESSION["logged_in"]) && isset($_POST["id"]) && isset($_POST["message_id"]))
 {
+	$link = connecteren();
 
 	fill_session($_SESSION["user_id"]);
-	get_colleague($_POST["id"]);
 
-	?>
+	$colleague_id = strip($_POST["id"]);
 
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<title>Sobu</title>
-		<link rel="shortcut icon" href="IMG/icon.png"/>
+	$message_id = strip($_POST["message_id"]);
 
-		<!-- Bootstrap -->
-		<link href="css/reset.css" rel="stylesheet" />
-		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet" />
+	$query_sended = "SELECT sender, receipant FROM `message` WHERE message_id = " .$message_id;
+	$result_sended = mysqli_query($link, $query_sended) or die("FOUT: er is een fout opgetreden bij het uitvoeren van de query \"$query_sended\"");
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<![endif]-->
-		</head>
-		<body>
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<a class="navbar-brand" href="index.php"><span class="so">So</span><span class="bu">bu</span></a>
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav navbar-right links_bovenaan">
-							<li><a href="messages_page.php">Messages</a></li>
-							<li><a href="settings_page.php">Settings</a></li>
-							<?php
+	if ($row = $result_sended->fetch_assoc()) 
+	{
+		
+	}
+	else
+	{
+		exit();
+	}
 
-							if ($_SESSION["admin"] == 1)
-							{
-								echo "<li><a href=\"admin_page.php\">Admin panel</a></li>";
-							}
+	if ($row["sender"] == $_SESSION["user_id"] || $row["receipant"] == $_SESSION["user_id"]) 
+	{
 
-							?>
-							<li><a href="logout.php">Logout</a></li>
-						</ul>
-					</div>
-				</div><!-- /.container-fluid -->
-			</nav>
+		get_colleague($colleague_id);
 
-			<div class="container-fluid">
-				<div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-5 col-md-offset-1 col-lg-4 col-lg-offset-2">
-					<div class="BOX margin_15_bottom">
-						<div class="col-md-5 user_foto">
-							<img src="<?php if($_SESSION["colleague"]["profile_picture"] != NULL) {echo "IMG/users/" .$_SESSION["profile_picture"];} else {echo "IMG/users/default.png";} ?>" alt="Profile picture" class="user_foto">
+		?>
+
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+			<title>Sobu</title>
+			<link rel="shortcut icon" href="IMG/icon.png"/>
+
+			<!-- Bootstrap -->
+			<link href="css/reset.css" rel="stylesheet" />
+			<link href="css/bootstrap.css" rel="stylesheet">
+			<link href="css/style.css" rel="stylesheet" />
+
+			<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+			<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+			<!--[if lt IE 9]>
+				<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+				<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+				<![endif]-->
+			</head>
+			<body>
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<a class="navbar-brand" href="index.php"><span class="so">So</span><span class="bu">bu</span></a>
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
 						</div>
-						<div class="col-md-7 user_info">
-							<ul>
-								<li><?php echo $_SESSION["colleague"]["name"]; ?></li>
-								<li><?php echo $_SESSION["colleague"]["function"]; ?></li>
-								<li>
-									<div class="col-xs-9 col-sm-9 col-md-9 no_pad_left">Online</div>
-									<div class="col-xs-3 col-sm-3 col-md-3">
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right links_bovenaan">
+								<li><a href="messages_page.php">Messages</a></li>
+								<li><a href="settings_page.php">Settings</a></li>
+								<?php
+
+								if ($_SESSION["admin"] == 1)
+								{
+									echo "<li><a href=\"admin_page.php\">Admin panel</a></li>";
+								}
+
+								?>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</div>
+					</div><!-- /.container-fluid -->
+				</nav>
+
+				<div class="container-fluid">
+					<div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-5 col-md-offset-1 col-lg-4 col-lg-offset-2">
+						<div class="BOX margin_15_bottom">
+							<div class="col-md-5 user_foto">
+								<img src="<?php if($_SESSION["colleague"]["profile_picture"] != NULL) {echo "IMG/users/" .$_SESSION["profile_picture"];} else {echo "IMG/users/default.png";} ?>" alt="Profile picture" class="user_foto">
+							</div>
+							<div class="col-md-7 user_info">
+								<ul>
+									<li><?php echo $_SESSION["colleague"]["name"]; ?></li>
+									<li><?php echo $_SESSION["colleague"]["function"]; ?></li>
+									<li>
+										<div class="col-xs-9 col-sm-9 col-md-9 no_pad_left">Online</div>
+										<div class="col-xs-3 col-sm-3 col-md-3">
 
 
-										<?php
+											<?php
 
-										if ($_SESSION["colleague"]["logged_in"] == 1)
-										{
-											echo "<img src=\"IMG/Green_circle.png\" title=\"Online\" alt=\"Online\" class=\"indicator_online_building\">";
-										}
-										else
-										{
-											echo "<img src=\"IMG/Red_circle.png\" title=\"Offline\" alt=\"Offline\" class=\"indicator_online_building\">";
-										}
+											if ($_SESSION["colleague"]["logged_in"] == 1)
+											{
+												echo "<img src=\"IMG/Green_circle.png\" title=\"Online\" alt=\"Online\" class=\"indicator_online_building\">";
+											}
+											else
+											{
+												echo "<img src=\"IMG/Red_circle.png\" title=\"Offline\" alt=\"Offline\" class=\"indicator_online_building\">";
+											}
 
-										?>
-									</div>
+											?>
+										</div>
 									<li>
 										<div class="col-xs-9 col-sm-9 col-md-9 no_pad_left">In Building</div>
 										<div class="col-xs-3 col-sm-3 col-md-3">
@@ -116,48 +137,47 @@ if (isset($_SESSION["logged_in"]) && isset($_POST["id"]))
 						<?php
 						if ($_SESSION["rights"]["info"] == 1)
 						{
-								echo "<div class='BOX margin_15_bottom no_pad_bottom'>
-												<div class='col-md-12'>
-												<a href=\"info_page_colleague.php?id=" .$_SESSION['colleague']['user_id']. "\" class='h4'>Info</a>
-												</div>
-											<p class='clear_both'></p>
-											</div>";
+							echo "<div class='BOX margin_15_bottom no_pad_bottom'>
+									<div class='col-md-12'>
+										<a href=\"info_page_colleague.php?id=" .$_SESSION['colleague']['user_id']. "\" class='h4'>Info</a>
+									</div>
+									<p class='clear_both'></p>
+								</div>";
 						}
 
 						if ($_SESSION["rights"]["check_in_out"] == 1)
 						{
-								echo "<div class='BOX margin_15_bottom no_pad_bottom'>
-												<div class='col-md-12'>
-												<a href='#' class='h4'>Check in and out history</a>
-												</div>
-											<p class='clear_both'></p>
-											</div>";
+							echo "<div class='BOX margin_15_bottom no_pad_bottom'>
+							<div class='col-md-12'>
+								<a href='#' class='h4'>Check in and out history</a>
+							</div>
+							<p class='clear_both'></p>
+						</div>";
 						}
 
 						if ($_SESSION["rights"]["messages"] == 1)
 						{
-								echo "<div class='BOX margin_15_bottom no_pad_bottom'>
-												<div class='col-md-12'>
-												<a href='#' class='h4'>Message and file history</a>
-												</div>
-											<p class='clear_both'></p>
-											</div>";
+							echo "<div class='BOX margin_15_bottom no_pad_bottom'>
+							<div class='col-md-12'>
+								<a href='#' class='h4'>Message and file history</a>
+							</div>
+							<p class='clear_both'></p>
+						</div>";
 						}
 						?>
 					</div>
 					<div class="col-xs-12 col-sm-5 col-sm-offset-0 col-md-5 col-md-offset-0 col-lg-4 user_search">
 						<div class="BOX">
-								<?php
-								$message_id = $_POST['message_id'];
-								$link = connecteren();
-								$query = "SELECT * FROM message WHERE message_id  = " .$message_id. "";
-								$result = mysqli_query($link, $query) or die("FOUT: er is een fout opgetreden bij het uitvoeren van de query \"$query\"");
-								if (mysqli_num_rows($result) == 1){
-									$result = mysqli_fetch_array($result);
-									echo "<h3>" .$result['topic']. "</h3><div class='col-md-12'><hr class='hr'></div><div class='col-md-12'>";
-									echo "".$result['content']." <a href=\"colleague_page.php?id=".$result['sender']."\">Reply</a></div>";
-								}
-								?>
+							<?php
+							$link = connecteren();
+							$query = "SELECT * FROM message WHERE message_id  = " .$message_id. "";
+							$result = mysqli_query($link, $query) or die("FOUT: er is een fout opgetreden bij het uitvoeren van de query \"$query\"");
+							if (mysqli_num_rows($result) == 1){
+								$result = mysqli_fetch_array($result);
+								echo "<h3>" .$result['topic']. "</h3><div class='col-md-12'><hr class='hr'></div><div class='col-md-12'>";
+								echo "".$result['content']." <a href=\"colleague_page.php?id=".$result['sender']."\">Reply</a></div>";
+							}
+							?>
 							<p class="clear_both"></p>
 							<p class="clear_both"></p>
 						</div>
@@ -171,10 +191,16 @@ if (isset($_SESSION["logged_in"]) && isset($_POST["id"]))
 				<!-- Include all compiled plugins (below), or include individual files as needed -->
 				<script src="js/bootstrap.min.js"></script>
 			</body>
-			</html>
-			<?php
-		}
-		else
-		{
-			header("Location: index.php");
-		}
+		</html>
+		<?php
+	}
+	else
+	{
+
+		header("Location: index.php");
+	}
+}
+else
+{
+	header("Location: index.php");
+}
